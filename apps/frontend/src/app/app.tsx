e68eba85c6d8be807ca39001e00e707b3852e909
sustainjane98 <jw19980309@gmail.com>
2026-03-09 12:24:05 +0100

    feat: add React Query Devtools for enhanced debugging experience

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -7,6 +7,7 @@ import {
 } from '@full-stack-test-task/shopping-list';
 import { ThemeProvider } from 'flowbite-react';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export function App() {
   return (
@@ -16,6 +17,7 @@ export function App() {
         <Routes>
           <Route path="/" element={<ShoppingList />} />
         </Routes>
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ThemeProvider>
   );
