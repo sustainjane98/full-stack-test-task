8d5b10b90f1eb0f8dd107e1ae66cfb648bcd8b51
sustainjane98 <jw19980309@gmail.com>
2026-03-10 11:30:52 +0100

    feat(empty-cart): update EmptyCartIcon styling for responsive design

--- libs/presentation/shopping-list/src/lib/components/empty-cart.tsx
+++ libs/presentation/shopping-list/src/lib/components/empty-cart.tsx
@@ -9,7 +9,7 @@ import { EmptyCartIcon } from './empty-cart-icon';
 export const EmptyCart: React.FC = () => {
   return (
     <div className="flex flex-1 justify-center items-center w-full h-full">
-      <EmptyCartIcon className="w-52" />
+      <EmptyCartIcon className="w-52 lg:w-72" />
     </div>
   );
 };
